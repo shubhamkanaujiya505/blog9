979094487768654ccf4329d7257f7f0c8142fad3
shubhamkanaujiya505@gmail.com <shubhamkanaujiya505@gmail.com>
2023-10-21 17:14:43 +0530

    Route Details

--- routes/web.php
+++ routes/web.php
@@ -3,29 +3,46 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-// Route::get('/{name}', function ($name) {
-//     return view('home',["name"=>$name]);
+Route::get('/home', function () {
+     return view('home'); 
+    // return "<h1>Welcome to home page</h1>"; // pass html tag also 
+});
+
+//some route command for help
+// php artisan
+// php artisan route -h
+
+// Available commands for the "route" namespace:
+
+//     route:cache  Create a route cache file for faster route registration
+//     route:clear  Remove the route cache file
+//     route:list   List all registered routes
+
+// php artisan route:list
+//php artisan route:list --except-vendor
+//php artisan route:list --path=home
+
+
+
+// We can pass multiple url also 
+// Route::get('/home/page', function () {
+//     // return view('home'); 
+//     return "<h1>Welcome to home page</h1>"; // pass html tag also 
 // });
-Route::view("/home","home");// alternate method calling page 
-// Route::get('/about', function () {
-//     return view('about');
-// });
-Route::view("/about","about");// alternate method calling page 
-// Route::get('/contact', function () {
-//     return view('contact');
-// });
-Route::view("/contact","contact"); // alternate method calling page 
+
+
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+//Another method to call the page 
+//Route::view("/about","about");// alternate method calling page 
+//Route::view("/home","home");// alternate method calling page 
+//Route::view("/contact","contact"); // alternate method calling page 
 
 
 
