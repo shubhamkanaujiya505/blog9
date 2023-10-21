9c35462c30c30291e87f2c214b7584e413bd4565
shubhamkanaujiya505@gmail.com <shubhamkanaujiya505@gmail.com>
2023-10-21 18:50:03 +0530

    Routes Group

--- routes/web.php
+++ routes/web.php
@@ -3,19 +3,42 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+// with group route
+Route::prefix('page')->group(function(){
+    Route::get('/homes', function () {
+        return "<h1>this is home page</h1>"; 
+    });
+    
+    Route::get('/about-us', function () {
+        return "<h1>this is about page</h1>"; 
+    });
+    
+    Route::get('/contact-us', function () {
+        return "<h1>this is contact page</h1>"; 
+    }); 
+});
+// when user type other name 
+Route::fallback(function(){
+    return "<h1>Page not found</h1>";
 
-Route::get('/homedfgh', function () {
-     return view('home'); 
-})->name('home');
+});
 
-Route::get('/aboutgsfdg', function () {
-     return view('about'); 
-})->name('about');
 
-Route::get('/contactdafdsf', function () {
-     return view('contact'); 
-})->name('contact'); // given name route 
 
+
+// Route::get('page/homes', function () {
+//     return view('home'); 
+// })->name('home');
+
+// Route::get('page/about-us', function () {
+//     return view('about'); 
+// })->name('about');
+
+// Route::get('page/contact-us', function () {
+//     return view('contact'); 
+// })->name('contact'); // given name route 
+
+// Route::redirect('/about','about-us'); // used when user bookmark last url but we change the url so we can redirect the new url using old url
 
 
 
