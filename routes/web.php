f3120074ae4b0e71fb116fa7bbe3d316149ec27c
shubhamkanaujiya505@gmail.com <shubhamkanaujiya505@gmail.com>
2023-10-21 17:51:49 +0530

    Routing Parameters & Constraints

--- routes/web.php
+++ routes/web.php
@@ -3,46 +3,53 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-
-Route::get('/home', function () {
-     return view('home'); 
-    // return "<h1>Welcome to home page</h1>"; // pass html tag also 
-});
-
-//some route command for help
-// php artisan
-// php artisan route -h
-
-// Available commands for the "route" namespace:
-
-//     route:cache  Create a route cache file for faster route registration
-//     route:clear  Remove the route cache file
-//     route:list   List all registered routes
-
-// php artisan route:list
-//php artisan route:list --except-vendor
-//php artisan route:list --path=home
+// validation on parameter pass
+// Route::get('/home/{id?}/comment/{comment?}', function (string $id = null, string $comment = null) {
+//         //  return view('home'); 
+//         return "<h1>Welcome to home parametric home page and id is:{$id} <br> comment is : {$comment}</h1>"; // pass html tag also 
+//     })->wherenumber('id')->whereAlpha('comment') ;
 
 
 
-// We can pass multiple url also 
-// Route::get('/home/page', function () {
-//     // return view('home'); 
-//     return "<h1>Welcome to home page</h1>"; // pass html tag also 
+// create own string or id 
+// Route::get('/home/{id?}/comment/{comment?}', function (string $id = null, string $comment = null) {
+//         //  return view('home'); 
+//         return "<h1>Welcome to home parametric home page and id is:{$id} <br> comment is : {$comment}</h1>"; // pass html tag also 
+//     })->wherein('id',[4,5,6,])->whereAlpha('comment',['hello','how','are','you']) ;
+
+
+// using regular expression 
+Route::get('/home/{id?}/comment/{comment?}', function (string $id = null, string $comment = null) {
+        //  return view('home'); 
+        return "<h1>Welcome to home parametric home page and id is:{$id} <br> comment is : {$comment}</h1>"; // pass html tag also 
+    })->where('id','[a-zA-Z]+')->where('id','[0-9]+');
+
+
+// pass multiple parameter 
+// Route::get('/home/{id?}/comment/{comment?}', function (string $id = null, string $comment = null) {
+//     //  return view('home'); 
+//     return "<h1>Welcome to home parametric home page and id is:{$id} and comment is : {$comment}</h1>"; // pass html tag also 
 // });
 
+// parameteric route pass defined type value 
+// ? => show this parameter is optional
+// Route::get('/home/{id?}', function (string $id = null) {
+//     //  return view('home'); 
+//     return "<h1>Welcome to home parametric home page and id is:{$id}</h1>"; // pass html tag also 
+// });
 
-Route::get('/about', function () {
-    return view('about');
-});
-Route::get('/contact', function () {
-    return view('contact');
-});
+// // parameteric route pass all type value 
 
-//Another method to call the page 
-//Route::view("/about","about");// alternate method calling page 
-//Route::view("/home","home");// alternate method calling page 
-//Route::view("/contact","contact"); // alternate method calling page 
+// Route::get('/home/{id}', function (string $id) {
+//     //  return view('home'); 
+//     return "<h1>Welcome to home parametric home page and id is:{$id}</h1>"; // pass html tag also 
+// });
+
+// simple route
+// Route::get('/home', function () {
+//      return view('home'); 
+//     // return "<h1>Welcome to home page</h1>"; // pass html tag also 
+// });
 
 
 
